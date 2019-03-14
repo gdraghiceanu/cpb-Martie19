5fd67c3de9ab961df632b51de9a76203ff6cdbdb
gdraghiceanu <george.draghiceanu@gmail.com>
2019-03-14 16:32:00 +0200

    prepare dev1

--- empapp/src/app/app.component.ts
+++ empapp/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-nav-bar></app-nav-bar>
   <app-emp-list></app-emp-list>
   `,
   styleUrls: ['./app.component.css']
--- empapp/src/app/app.module.ts
+++ empapp/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employees/employee-list.component';
-import { EmployeeComponent } from './employees/employee.component';
-import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    EmployeeComponent,
-    NavBarComponent
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
