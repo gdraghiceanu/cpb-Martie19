d157ee5733569091c5e349cb4534c55ce24467e6
Cosmin Serban <cosmin@creative-ones.com>
2019-03-14 21:14:04 +0200

    curs 2

--- empapp/src/app/employees/employee-list.component.ts
+++ empapp/src/app/employees/employee-list.component.ts
@@ -5,6 +5,57 @@ import { Component } from '@angular/core';
     templateUrl: './employee-list.component.html'
 })
 export class EmployeeListComponent {
+    text:string;
 
+    employees =       [
+        {
+        "userId":"rirani",
+        "jobTitleName":"Developer",
+        "firstName":"Romin",
+        "lastName":"Irani",
+        "employeeCode":"E1",
+        "region":"CA",
+        "phoneNumber":"408-1234567",
+        "emailAddress":"romin.k.irani@gmail.com",    
+        "location": {
+                            "address": "1057 DT",
+                            "city": "London",
+                            "country": "England"
+                          },
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        },
+        {
+        "userId":"nirani",
+        "jobTitleName":"Developer",
+        "firstName":"Neil",
+        "lastName":"Irani",
+        "employeeCode":"E2",
+        "region":"CA",
+        "phoneNumber":"408-1111111",
+        "emailAddress":"neilrirani@gmail.com",
+        "location": {
+            "address": "1057 DT",
+            "city": "London",
+            "country": "England"
+          },
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        },
+        {
+        "userId":"thanks",
+        "jobTitleName":"Program Directory",
+        "firstName":"Tom",
+        "lastName":"Hanks",
+        "employeeCode":"E3",
+        "region":"CA",
+        "phoneNumber":"408-2222222",
+        "emailAddress":"tomhanks@gmail.com",
+        "onlineContact":"ceva",
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        }
+    ]
+
+    handleEventFromChild(val: string){
+            alert(val);
+    }
 }
 
