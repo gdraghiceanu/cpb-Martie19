701fafc01277a78b29446ced553b51c18e93af58
unknown <george.draghiceanu@gmail.com>
2019-03-14 21:12:36 +0200

    curs 2

--- empapp/src/app/app.component.ts
+++ empapp/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
+  template: `  
   <app-emp-list></app-emp-list>
   `,
   styleUrls: ['./app.component.css']
--- empapp/src/app/employees/employee-list.component.ts
+++ empapp/src/app/employees/employee-list.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
 })
 export class EmployeeListComponent {
 
+    text: string;
+
+    employees =  [
+        {
+        "userId":"rirani",
+        "jobTitleName":"Developer",
+        "firstName":"Romin",
+        "lastName":"Irani",
+        "employeeCode":"E1",
+        "region":"CA",
+        "phoneNumber":"408-1234567",
+        "emailAddress":"romin.k.irani@gmail.com", 
+        "onlineContact" : "www.mysite.com",        
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        },
+        {
+        "userId":"nirani",
+        "jobTitleName":"Developer",
+        "firstName":"Neil",
+        "lastName":"Irani",
+        "employeeCode":"E2",
+        "region":"CA",
+        "phoneNumber":"408-1111111",
+        "emailAddress":"neilrirani@gmail.com",
+        "location": {
+            "address": "1057 DT",
+            "city": "London",
+            "country": "England"
+          },
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        },
+        {
+        "userId":"thanks",
+        "jobTitleName":"Program Directory",
+        "firstName":"Tom",
+        "lastName":"Hanks",
+        "employeeCode":"E3",
+        "region":"CA",
+        "phoneNumber":"408-2222222",
+        "emailAddress":"tomhanks@gmail.com",
+        "location": {
+            "address": "1057 DT",
+            "city": "London",
+            "country": "England"
+          },
+        "imageUrl": "/assets/images/angularconnect-shield.png"
+        }
+    ];       
+
+handleEveFromChild(val: any) {
+    this.text = val;
+}
+
 }
 
