607f6017a706784ccf66a341689df1a45e7981c8
ENDAVA\cabadea <cristian.badea@endava.com>
2019-05-02 10:35:56 +0300

    add requisites for step 2

---
+++ empapp/requisites/curs8/2-form-component-certs.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, FormArray, FormBuilder, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-create-employee',
+  templateUrl: './reactive-create-employee.component.html',
+  styleUrls: ['./reactive-create-employee.component.css']
+})
+export class ReactiveCreateEmployeeComponent implements OnInit {
+  jobs: string[] = ['Developer', 'Programm Directory', 'Project Manager', 'Business Analyst', 'CEO', 'Office Manager'];
+  regions: string[] = ['Bucuresti', 'Constanta', 'Cluj', 'Timisoara', 'Sibiu', 'Iasi', 'Suceava'];
+
+  employeeForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    jobTitle: new FormControl(''),
+    code: new FormControl(''),
+    region: new FormControl(''),
+    salary: new FormControl(''),
+    certificates: new FormArray([new FormControl()])
+  });
+
+  get certificates(): FormArray {
+    return this.employeeForm.get('certificates') as FormArray;
+  }
+
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {}
+
+  cancel() {
+    this.route.navigate(['/employees']);
+  }
+
+  saveEmployee() {}
+
+  addCertificate(): void {
+    this.certificates.push(new FormControl());
+  }
+}
