9f706652208905511c21d29393b3b1a67e2974f6
ENDAVA\cabadea <cristian.badea@endava.com>
2019-05-13 18:39:15 +0300

    update reactive create employee form to last stage + add ViewChild example to template-create-employee

--- empapp/src/app/employees/reactive-create-employee/reactive-create-employee.component.ts
+++ empapp/src/app/employees/reactive-create-employee/reactive-create-employee.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormArray,
+  FormBuilder,
+  Validators,
+  FormControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-create-employee',
@@ -25,7 +32,48 @@ export class ReactiveCreateEmployeeComponent implements OnInit {
     'Suceava'
   ];
 
-  constructor(private route: Router) {}
+  employeeForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    jobTitle: ['', Validators.required],
+    code: [
+      '',
+      [Validators.required, Validators.pattern('[a-zA-Z]{1,3}-[0-9]{1,3}')]
+    ],
+    region: ['', Validators.required],
+    salary: ['', [Validators.required, Validators.min(900)]],
+    certificates: this.formBuilder.array([this.createCertificateGroup()])
+  });
+
+  get firstName(): FormControl {
+    return this.employeeForm.get('firstName') as FormControl;
+  }
+
+  get lastName(): FormControl {
+    return this.employeeForm.get('lastName') as FormControl;
+  }
+
+  get jobTitle(): FormControl {
+    return this.employeeForm.get('jobTitle') as FormControl;
+  }
+
+  get code(): FormControl {
+    return this.employeeForm.get('code') as FormControl;
+  }
+
+  get region(): FormControl {
+    return this.employeeForm.get('region') as FormControl;
+  }
+
+  get salary(): FormControl {
+    return this.employeeForm.get('salary') as FormControl;
+  }
+
+  get certificates(): FormArray {
+    return this.employeeForm.get('certificates') as FormArray;
+  }
+
+  constructor(private route: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 
@@ -33,5 +81,26 @@ export class ReactiveCreateEmployeeComponent implements OnInit {
     this.route.navigate(['/employees']);
   }
 
-  saveEmployee() {}
+  saveEmployee() {
+    alert(JSON.stringify(this.employeeForm.value));
+  }
+
+  addCertificate(): void {
+    this.certificates.push(this.createCertificateGroup());
+  }
+
+  ifControlRequired(formControl: FormControl): boolean {
+    return (
+      formControl.touched &&
+      formControl.invalid &&
+      formControl.errors['required']
+    );
+  }
+
+  private createCertificateGroup(): FormGroup {
+    return this.formBuilder.group({
+      name: [''],
+      year: ['']
+    });
+  }
 }
--- empapp/src/app/employees/template-create-employee/create-employee.component.ts
+++ empapp/src/app/employees/template-create-employee/create-employee.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Employee } from '../../models/employee';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
-export class CreateEmployeeComponent implements OnInit {
+export class CreateEmployeeComponent implements OnInit, AfterViewInit, OnDestroy {
+  // ViewChild gets template reference when available
+  // Both ViewChilds are of type NgModel because we set on the template the ref equaled to the ngModel
+  // NgModel is a directive so we get an object of type NgModel
+  // inside we can find the reference to our FormControl
 
+  // fnvar will be rendered from the start
+  @ViewChild('fnvar') firstNameInput: NgModel;
+
+  // lnvar will be rendered when isVisible==true
+  @ViewChild('lnvar') lastNameInput: NgModel;
+
+  private firstNameSubscription: Subscription;
+  private lastNameSubscription: Subscription;
+
+  isVisible = false;
   isValid = true;
   employee: Employee;
   isJobsError: boolean;
@@ -16,12 +31,36 @@ export class CreateEmployeeComponent implements OnInit {
   isRegionError: boolean;
   regions: string[] = ['Bucuresti', 'Constanta', 'Cluj', 'Timisoara', 'Sibiu', 'Iasi', 'Suceava'];
 
-  constructor(private route: Router) { }
+  constructor(private route: Router) {}
 
   ngOnInit() {
     this.employee = new Employee();
     this.employee.jobTitleName = 'default';
     this.employee.region = 'default';
+
+    // we simulate a slow http call. We want to dispaly the input only after we received the data
+    setTimeout(() => {
+      this.isVisible = true;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.firstNameSubscription = this.firstNameInput.control.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+
+    this.lastNameSubscription = this.lastNameInput.control.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.firstNameSubscription) {
+      this.firstNameSubscription.unsubscribe();
+    }
+    if (this.lastNameSubscription) {
+      this.lastNameSubscription.unsubscribe();
+    }
   }
 
   cancel() {
@@ -47,5 +86,4 @@ export class CreateEmployeeComponent implements OnInit {
   saveEmployee(empForm: NgForm) {
     console.log(empForm);
   }
-
 }
