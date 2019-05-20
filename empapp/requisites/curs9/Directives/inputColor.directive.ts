18231d2bdba556d4492d2bd6ce974455f16c4331
Cristian Alexandru Victor Badea <badea_cristian_alexandru@yahoo.com>
2019-05-20 23:20:30 +0300

    add hostListener directive example

---
+++ empapp/requisites/curs9/Directives/inputColor.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Directive({
+    selector: '[inputColor]'
+})
+export class InputColorDirective {
+  constructor(private elementRef: ElementRef<HTMLInputElement>) {}
+  @Input() inputFormControl: FormControl;
+
+  @HostListener('mouseenter') onmouseenter() {
+    this.borderHighlight(true);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.borderHighlight(false);
+  }
+
+  borderHighlight(shouldHighlight: boolean) {
+    if (shouldHighlight) {
+      if (this.inputFormControl.valid) {
+        this.elementRef.nativeElement.style.border = '5px solid green';
+      } else {
+        this.elementRef.nativeElement.style.border = '5px solid red';
+      }
+    } else {
+      this.elementRef.nativeElement.style.border = 'initial';
+    }
+  }
+}
