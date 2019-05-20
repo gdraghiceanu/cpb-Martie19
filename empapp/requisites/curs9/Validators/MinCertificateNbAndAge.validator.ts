d0a02892696eea1c7de43e05b09fefe38aa0bc19
Cristian Alexandru Victor Badea <badea_cristian_alexandru@yahoo.com>
2019-05-20 09:21:10 +0300

    add formArray validator

--- empapp/requisites/curs9/Validators/MinCertificateNbAndAge.validator.ts
+++ empapp/requisites/curs9/Validators/MinCertificateNbAndAge.validator.ts
@@ -1,7 +1,17 @@
 import { ValidatorFn, FormArray } from '@angular/forms';
 
-export function MinCertificateNbAndAgeValidator(certificateNumber: number, certificateAgeYears: number): ValidatorFn {
+export function MinCertificateNbAndAgeValidator(
+  certMinNumber: number,
+  certMaxAgeYears: number,
+  certYearKey: string
+): ValidatorFn {
   return (control: FormArray): { [key: string]: any } | null => {
-    return null;
+    const year = new Date().getFullYear();
+    const recentCertificates = (control.value as object[]).filter(
+      c => year - c[certYearKey] <= certMaxAgeYears
+    );
+    return recentCertificates.length >= certMinNumber
+      ? null
+      : { minCertificateNbAndAgeValidator: 'not enough recent certificates' };
   };
 }
